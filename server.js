96ab7cb0a5ebc57c0b5b9d8ab5c82b7185bdb490
Ahmed <ahmedksontini122@gmail.com>
2026-04-15 22:54:01 +0000

    server

--- server.js
+++ server.js
@@ -8,7 +8,11 @@ const app = express();
 
 connectDB();
 
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:53311/'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
